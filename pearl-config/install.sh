d87d271f060892a8034af8e0a742dcc40dac6212
Filippo Squillace <squillace.filippo@gmail.com>
2018-04-10 09:06:09 +1000

    Add Keyboard configuration

--- pearl-config/install.sh
+++ pearl-config/install.sh
@@ -12,11 +12,13 @@ post_install() {
 
     # Apply custom configurations
     mkdir -p $PEARL_PKGVARDIR/configs/
-    ask "Do you want to follow procedure for input configuration (i.e. mouse, etc)?" "N" && \
+    ask "Do you want to follow procedure for input configuration (i.e. mouse, keyboard, etc)?" "N" && \
         _configure_input
 
     # Apply default configuration files
     apply "source $PEARL_PKGDIR/xinitrc" "$HOME/.xinitrc"
+    # The following makes sure to place Awesome WM at the end
+    apply "exec awesome" "$HOME/.xinitrc" false
 
     # Information and manual changes
     info "Following steps requires manual changes"
@@ -29,9 +31,16 @@ post_install() {
 }
 
 _configure_input() {
+    local xinitrc_input_file="$PEARL_PKGVARDIR/configs/xinitrc-input"
+
+    info "More info about Keyboard config:"
+    info "    https://wiki.archlinux.org/index.php/Keyboard_configuration_in_Xorg#Setting_keyboard_layout"
+    info "For full list of keyboard models take a look at: /usr/share/X11/xkb/rules/base.lst"
+    local kblayout=$(input "Provide the language Keyboard layout" "us")
+    echo "setxkbmap -layout $kblayout" >> "$xinitrc_input_file"
+
     info "More info about how to config mouse acceleration:"
     info "    https://wiki.archlinux.org/index.php/Mouse_acceleration"
-    local xinitrc_input_file="$PEARL_PKGVARDIR/configs/xinitrc-input"
     echo "" > "$xinitrc_input_file"
     while ask "Do you want to configure additional input?" "N"
     do
@@ -50,6 +59,7 @@ _configure_input() {
             echo "xinput --set-prop '$input_name' '$prop_name' '$value'" >> "$xinitrc_input_file"
         }
     done
+
     apply "source $xinitrc_input_file" "$HOME/.xinitrc"
 
     return 0
@@ -60,6 +70,7 @@ post_update() {
 }
 
 pre_remove() {
+    unapply "exec awesome" "$HOME/.xinitrc"
     local xinitrc_input_file="$PEARL_PKGVARDIR/configs/xinitrc-input"
     unapply "source $xinitrc_input_file" "$HOME/.xinitrc"
     unapply "source $PEARL_PKGDIR/xinitrc" "$HOME/.xinitrc"
