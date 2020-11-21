a7604601e8af4eb5602c75409840d70ea4db6e14
Filippo Squillace <squillace.filippo@gmail.com>
2020-11-21 16:44:04 +0100

    Add pass, font and spell checking configs to qutebrowser

--- configs/qutebrowser_config.py
+++ configs/qutebrowser_config.py
@@ -830,56 +830,73 @@ c.auto_save.session = True
 
 ## Font used in the completion categories.
 ## Type: Font
-# c.fonts.completion.category = 'bold 10pt monospace'
+# c.fonts.completion.category = 'bold default_size default_family'
 
 ## Font used in the completion widget.
 ## Type: Font
-# c.fonts.completion.entry = '10pt monospace'
+# c.fonts.completion.entry = 'default_size default_family'
+
+## Font used for the context menu. If set to null, the Qt default is
+## used.
+## Type: Font
+# c.fonts.contextmenu = None
 
 ## Font used for the debugging console.
-## Type: QtFont
-# c.fonts.debug_console = '10pt monospace'
+## Type: Font
+# c.fonts.debug_console = 'default_size default_family'
+
+## Default font families to use. Whenever "default_family" is used in a
+## font setting, it's replaced with the fonts listed here. If set to an
+## empty value, a system-specific monospace default is used.
+## Type: List of Font, or Font
+# c.fonts.default_family = []
+
+## Default font size to use. Whenever "default_size" is used in a font
+## setting, it's replaced with the size listed here. Valid values are
+## either a float value with a "pt" suffix, or an integer value with a
+## "px" suffix.
+## Type: String
+# c.fonts.default_size = '10pt'
 
 ## Font used for the downloadbar.
 ## Type: Font
-# c.fonts.downloads = '10pt monospace'
+# c.fonts.downloads = 'default_size default_family'
 
 ## Font used for the hints.
 ## Type: Font
-# c.fonts.hints = 'bold 10pt monospace'
+# c.fonts.hints = 'bold default_size default_family'
 
 ## Font used in the keyhint widget.
 ## Type: Font
-# c.fonts.keyhint = '10pt monospace'
+# c.fonts.keyhint = 'default_size default_family'
 
 ## Font used for error messages.
 ## Type: Font
-# c.fonts.messages.error = '10pt monospace'
+# c.fonts.messages.error = 'default_size default_family'
 
 ## Font used for info messages.
 ## Type: Font
-# c.fonts.messages.info = '10pt monospace'
+# c.fonts.messages.info = 'default_size default_family'
 
 ## Font used for warning messages.
 ## Type: Font
-# c.fonts.messages.warning = '10pt monospace'
-
-## Default monospace fonts. Whenever "monospace" is used in a font
-## setting, it's replaced with the fonts listed here.
-## Type: Font
-# c.fonts.monospace = '"xos4 Terminus", Terminus, Monospace, "DejaVu Sans Mono", Monaco, "Bitstream Vera Sans Mono", "Andale Mono", "Courier New", Courier, "Liberation Mono", monospace, Fixed, Consolas, Terminal'
+# c.fonts.messages.warning = 'default_size default_family'
 
 ## Font used for prompts.
 ## Type: Font
-# c.fonts.prompts = '10pt sans-serif'
+# c.fonts.prompts = 'default_size sans-serif'
 
 ## Font used in the statusbar.
 ## Type: Font
-# c.fonts.statusbar = '10pt monospace'
+# c.fonts.statusbar = 'default_size default_family'
 
-## Font used in the tab bar.
-## Type: QtFont
-# c.fonts.tabs = '10pt monospace'
+## Font used for selected tabs.
+## Type: Font
+# c.fonts.tabs.selected = 'default_size default_family'
+
+## Font used for unselected tabs.
+## Type: Font
+# c.fonts.tabs.unselected = 'default_size default_family'
 
 ## Font family for cursive fonts.
 ## Type: FontFamily
@@ -921,6 +938,20 @@ c.auto_save.session = True
 ## out.
 ## Type: Int
 # c.fonts.web.size.minimum_logical = 6
+
+# Default font families to use. Whenever "default_family" is used in a
+# font setting, it's replaced with the fonts listed here. If set to an
+# empty value, a system-specific monospace default is used.
+# Type: List of Font, or Font
+c.fonts.default_family = ["TerminessTTF Nerd Font"]
+
+# Default font size to use. Whenever "default_size" is used in a font
+# setting, it's replaced with the size listed here. Valid values are
+# either a float value with a "pt" suffix, or an integer value with a
+# "px" suffix.
+# Type: String
+c.fonts.default_size = '14pt'
+
 
 ## When a hint can be automatically followed without pressing Enter.
 ## Type: String
@@ -1245,7 +1276,7 @@ c.auto_save.session = True
 ##   - tr-TR: Turkish (Turkey)
 ##   - uk-UA: Ukrainian (Ukraine)
 ##   - vi-VN: Vietnamese (Viet Nam)
-# c.spellcheck.languages = []
+c.spellcheck.languages = ['en-US', 'es-ES', 'it-IT']
 
 ## Hide the statusbar unless a message is shown.
 ## Type: Bool
@@ -1839,6 +1870,10 @@ config.bind('yy', 'yank ;; yank -s')
 # config.bind('n', 'prompt-accept no', mode='yesno')
 # config.bind('y', 'prompt-accept yes', mode='yesno')
 
-config.bind('m', 'spawn umpv {url}')
-config.bind('M', 'hint links spawn umpv {hint-url}')
-config.bind(';M', 'hint --rapid links spawn umpv {hint-url}')
+# Use comma for custom bindings
+config.bind(',m', 'spawn umpv {url}')
+config.bind(',M', 'hint links spawn umpv {hint-url}')
+config.bind(',n', 'hint --rapid links spawn umpv {hint-url}')
+
+config.bind(',p', 'spawn --userscript qute-pass')
+config.bind(',P', 'spawn --userscript qute-pass --password-only')
